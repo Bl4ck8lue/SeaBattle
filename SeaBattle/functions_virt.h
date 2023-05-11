2d49c480d4d393eaf4e0ad7eb1026a917543b772
Илья Илюхин <XLORDD@yandex.ru>
2023-05-11 17:05:55 +0300

    Созданы заголовки функций компьютера

---
+++ SeaBattle/functions_virt.h
@@ -0,0 +1,5 @@
+#pragma once
+char Show_Place_Virt(char massive_virt[10][10]);
+char Virtial_Check_And_Move(char(*)[10], int x, int y, int check_numb);
+char Virtual_Move(char(*)[10]);
+int Virtual_Shoot(char(*)[10], int);
